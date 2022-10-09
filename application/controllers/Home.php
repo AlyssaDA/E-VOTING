1b0546f592cf8dbee5736b5a987f8d8d69423e49
ferijuliana11 <114659175+ferijuliana11@users.noreply.github.com>
2022-10-09 15:15:49 +0700

    Update Home.php Feri

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -11,17 +11,34 @@ public function index()
 {
     $data['title'] = 'Home';
     $data['kandidat'] = $this->db->get('kandidat')->result();
+    $data['kandidat1'] = $this->db->get_where('suara', ['nama_kandidat' => 'Calon ke-1'])->num_rows();
+    $data['kandidat2'] = $this->db->get_where('suara', ['nama_kandidat' => 'Calon ke-2'])->num_rows();
+    $data['kandidat3'] = $this->db->get_where('suara', ['nama_kandidat' => 'Calon ke-3'])->num_rows();
+    $data['user'] = $this->db->get_where('user', ['id' => $this->session->userdata('id')])->row();
     $this->load->view('templates/header', $data);
     $this->load->view('home', $data);
     $this->load->view('templates/footer');
 }
 
-public function visimisi()
+public function visimisi($id_kandidat)
 {
     $data['title'] = 'Visi & Misi';
-    // $data['kandidat'] = $this->db->get('kandidat')->result();
+    $data['kandidat'] = $this->db->get_where('kandidat', ['id' => $id_kandidat])->row();
+    $data['visimisi'] = $this->db->get_where('visimisi', ['id_kandidat' => $id_kandidat])->row();
     $this->load->view('templates/header', $data);
     $this->load->view('visimisi', $data);
     $this->load->view('templates/footer');
 }
+public function pilih_kandidat()
+{
+    $this->HomeModel->pilih_kandidat();
+    $this->HomeModel->update_status_user();
+    if ($this->db->affected_rows() > 0) {
+        $result = ['success' => true];
+    }else {
+        $result = ['success' =>false];
+    }
+    
+    echo json_encode($result);
+    }
 }
